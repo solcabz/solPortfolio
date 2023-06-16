6bc1287d3412203a59bda9bd80e632943636b86b
solcabz <41091153+solcabz@users.noreply.github.com>
2023-06-16 13:24:34 +0800

    new

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    var docEl = $(document),
+        headerEl = $('div'),
+        headerWrapEl = $('.wrap-nav'),
+        navEl = $('ul'),
+        linkScroll = $('.scroll');
+    
+    docEl.on('scroll', function(){
+      if ( docEl.scrollTop() > 60 ){
+        headerEl.addClass('fixed-to-top');
+        headerWrapEl.addClass('fixed-to-top');
+        navEl.addClass('fixed-to-top');
+      }
+      else {
+        headerEl.removeClass('fixed-to-top');
+        headerWrapEl.removeClass('fixed-to-top');
+        navEl.removeClass('fixed-to-top');
+      }
+    });
+    
+    linkScroll.click(function(e){
+        e.preventDefault();
+        $('body, html').animate({
+           scrollTop: $(this.hash).offset().top
+        }, 500);
+     });
+  });
+
+  const hamburger = document.querySelector(".menu-btn");
+  const navMenu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
